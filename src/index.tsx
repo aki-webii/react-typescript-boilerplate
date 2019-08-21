caeacab0999433b588b01a6757298264500186df
aki-webii <c.mikamichel@gmail.com>
2019-08-21 14:47:44 +0900

    chore: Intorduce minireset.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./components/App";
+import "minireset.css";
 // import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("app"));
